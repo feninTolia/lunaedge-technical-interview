c60f1daeb1b74144a6cd7e27f3b4689d53fee687
Anatolii Fenin <fenintolja@gmail.com>
2023-12-21 09:47:37 +0200

    feat: dependencies setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
 function App() {
   return (
     <div>
       <p>Welcome to Luna Edge technical interview</p>
+      <h1 className="text-3xl font-bold underline text-red-500">
+        Hello world!
+      </h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
