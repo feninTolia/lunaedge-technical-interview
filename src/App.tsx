07f9286a31639a2ec070253b6fbf51c327fb871f
Anatolii Fenin <fenintolja@gmail.com>
2023-12-21 11:05:00 +0200

    feat: pokemon base service

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
-import { useState } from 'react';
+import { useEffect } from 'react';
 import Input from './shared/ui/Input';
 import MultipleSelect from './shared/ui/MultipleSelect';
 import Badge from './shared/ui/Badge';
+import { pokemonService } from './shared/service/pokemon.service';
 
 function App() {
+  const fetchPoke = async () => {
+    await pokemonService.fetchAllPokemons();
+  };
+
+  useEffect(() => {
+    fetchPoke();
+  }, []);
+
   return (
     <div>
       <p>Welcome to Luna Edge technical interview</p>
       <Input />
       <MultipleSelect />
       <Badge>Badge</Badge>
-      <Badge>Badge asasas asdasd asdasd</Badge>
     </div>
   );
 }
---
+++ src/shared/service/pokemon.service.ts
@@ -0,0 +1,31 @@
+import axios, { AxiosResponse } from 'axios';
+
+class PokemonService {
+  private apiUrl = 'https://pokeapi.co/api/v2/pokemon/';
+
+  private handleError(error: any): void {
+    console.error('API Error:', error);
+    throw error;
+  }
+
+  private async fetchData(url: string): Promise<any> {
+    try {
+      const response: AxiosResponse = await axios.get(url);
+      return response.data;
+    } catch (error) {
+      this.handleError(error);
+    }
+  }
+
+  fetchAllPokemons = async (limit = 20): Promise<any> => {
+    const url = `${this.apiUrl}?limit=${limit}`;
+    return this.fetchData(url);
+  };
+
+  fetchPokeByFullName = async (fullName: string): Promise<any> => {
+    const url = `${this.apiUrl}${fullName}`;
+    return this.fetchData(url);
+  };
+}
+
+export const pokemonService = new PokemonService();
